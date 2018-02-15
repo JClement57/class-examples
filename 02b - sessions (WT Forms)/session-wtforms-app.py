158f41416f8eac45310814fa53c32a6ad375a35e
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2018-02-15 10:21:40 -0500

    Update WTForms example
    - Add fields with more types and different validators
    - Style error messages
    - Improve session keys
    - Style form with Bootstrap

--- 02b - sessions (WT Forms)/session-wtforms-app.py
+++ 02b - sessions (WT Forms)/session-wtforms-app.py
@@ -1,21 +1,25 @@
 from flask import Flask, session, redirect, url_for, render_template, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, BooleanField, PasswordField
 from wtforms.validators import Email, Length
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret key for session application'
+
 
 # Default route shows a simple home page.
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Request a user name and password. Doesn't fiddle with hiding the password, etc.
+
+# Log in form with validation.
 class LoginForm(FlaskForm):
-    username = StringField('Email', validators=[Length(min=1, max=40)])
-    password = StringField('Password', validators=[Length(min=1, max=40)])
+    email = StringField('Email', validators=[Email()])
+    password = PasswordField('Password', validators=[Length(min=6, max=40)])
+    remember = BooleanField('Remember me on this machine', default=False)
     submit = SubmitField('Log In')
+
 
 # Log in
 @app.route('/login', methods=['GET', 'POST'])
@@ -32,8 +36,9 @@ def login():
         else:
             # Correct password. Add a value to the session object
             # to show that the user is logged in. Redirect to home page.
-            session['username'] = login_form.username.data
-            flash('User {} logged in'.format(session['username']))
+            session['email'] = login_form.email.data
+            session['remember'] = login_form.remember.data
+            flash('User {} logged in'.format(session['email']))
             return redirect(url_for('index'))
 
     # Render the form if:
@@ -42,18 +47,21 @@ def login():
     # 3. The form validated but the password was wrong.
     return render_template('login.html', form=login_form)
 
+
 # Log out
 @app.route('/logout')
 def logout():
-    # Remove the 'username' entry from the session.
+    # Remove the 'email' entry from the session.
     # The pop() method behaves as follows:
-    # 1. If 'username' is in the session, remove it and return its value.
+    # 1. If 'email' is in the session, remove it and return its value.
     #    The value will be the user name stored there by the login() view function.
-    #    Removing the username from the session has the effect of logging out the user.
-    # 2. If 'username' is not in the session, return the second argument (None)
-    user_name = session.pop('username', None)
+    #    Removing the email from the session has the effect of logging out the user.
+    # 2. If 'email' is not in the session, return the second argument (None)
+    session.pop('remember', None)
+    user_name = session.pop('email', None)
     flash('User {} logged out'.format(user_name))
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
